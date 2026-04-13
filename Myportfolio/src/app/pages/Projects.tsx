be7f4cf18b0fd9e8de8a78b73afaeb01285feef9
Thuy Nguyen <thuyn18@uci.edu>
2026-04-13 14:39:44 -0700

    fix bug in project

--- Myportfolio/src/app/pages/Projects.tsx
+++ Myportfolio/src/app/pages/Projects.tsx
@@ -15,7 +15,7 @@ export function Projects() {
     {
       title: 'Data Science (Capstone Project)',
       period: 'March 2026 - Present',
-      technologies: [Python, numpy, pandas, matplotlib, skicit-learn, PostgreSQL]
+      technologies: [Python, numpy, pandas, matplotlib, skicit-learn, PostgreSQL],
       responsibilities: [
         'Spearheaded data cleaning and preprocessing for 20,000+ surgical records, handling null values via median imputation and targeted deletion to ensure high data quality for predictive modeling.',
         'Conducted comprehensive Exploratory Data Analysis (EDA) using Python (NumPy, Pandas, Matplotlib, Missingno) to visualize 90+ feature distributions, target variables, and uncover key data trends.',
